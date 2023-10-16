c833b173398489fd7fe243b5f12a5e708088e78b
Jehwan Yoo <yoojehwan1875@gmail.com>
2023-10-16 15:42:07 +0900

    feat: level 2

---
+++ src/2.infinity-test.js
@@ -0,0 +1,21 @@
+import { describe, it } from 'node:test'
+
+describe('infinity function', () => {
+  it.todo('should increment by 1 for each call, starting from 1, over 10 calls')
+
+  it.todo('should start from 0 by default when the start parameter is not provided')
+
+  it.todo('should decrement by 1 for each call, starting from -1, to -10 when a decrement function is provided')
+})
+
+describe('limit function', () => {
+  it.todo('should limit n elements from an iterable object')
+
+  it.todo('should throw a TypeError if n is less than 0')
+})
+
+describe('odds function', () => {
+  it.todo('should only yield odd values when called')
+
+  it.todo('should throw a TypeError if n is less than or equal to 0')
+})
---
+++ src/2.infinity.js
@@ -0,0 +1,5 @@
+export function* infinity(start) {}
+
+export function* limit(n, iterable) {}
+
+export function* odds(n) {}

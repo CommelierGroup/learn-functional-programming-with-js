04f755792137053098d9a997394dd521dbcf9c97
Jehwan Yoo <yoojehwan1875@gmail.com>
2023-10-16 15:42:04 +0900

    feat: level 1

---
+++ src/1.iteration-test.js
@@ -0,0 +1,19 @@
+import { describe, it } from 'node:test'
+
+describe('Array, Set, Map', () => {
+  it.todo('should be iterable')
+
+  it.todo('should have the well-formed iterator')
+})
+
+describe('iteration protocols', () => {
+  it.todo('should be iterable')
+
+  it.todo('should have the well-formed iterator')
+})
+
+describe('generator', () => {
+  it.todo('should be iterable')
+
+  it.todo('should have the well-formed iterator')
+})
---
+++ src/1.iteration.js
@@ -0,0 +1,5 @@
+export const iterable = {}
+
+export const checkIterable = target => {}
+
+export const checkIterableIterator = target => {}

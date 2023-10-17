402b27a153c6edd13aabcdacd6c2386a4d4a2b63
Jehwan Yoo <yoojehwan1875@gmail.com>
2023-10-17 11:30:59 +0900

    test: should perform pow(0), pow(1), pow(2), ..., pow(5)

--- src/0.higher-order-function-test.js
+++ src/0.higher-order-function-test.js
@@ -1,6 +1,6 @@
 import { describe, it } from 'node:test'
 import * as assert from 'node:assert'
-import { makeAddN } from './0.higher-order-function.js'
+import { makeAddN, times } from './0.higher-order-function.js'
 
 describe('makeAddN', () => {
   it('should create a closure that performs x + 2', () => {
@@ -15,5 +15,10 @@ describe('makeAddN', () => {
 })
 
 describe('times', () => {
-  it.todo('should perform pow(0), pow(1), pow(2), ..., pow(5)')
+  it('should perform pow(0), pow(1), pow(2), ..., pow(5)', () => {
+    const want = [0, 1, 4, 9, 16, 25]
+    const got = times(n => Math.pow(n, 2), 6)
+
+    assert.deepEqual(want, got)
+  })
 })

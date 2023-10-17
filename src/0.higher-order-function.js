5243d6fd59110bb64befb4128cdb4627bb852dab
Jehwan Yoo <yoojehwan1875@gmail.com>
2023-10-17 11:31:19 +0900

    feat: times function

--- src/0.higher-order-function.js
+++ src/0.higher-order-function.js
@@ -2,4 +2,10 @@ export const makeAddN = (fn, n) => {
   return x => fn(x, n)
 }
 
-export const times = (fn, n) => {}
+export const times = (fn, n) => {
+  const result = []
+  for (let i = 0; i < n; i++) {
+    result.push(fn(i))
+  }
+  return result
+}

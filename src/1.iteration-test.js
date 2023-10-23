9bf805b9f3e29a1ca8bfa0de94d08fb1edd5e0d8
Jehwan Yoo <yoojehwan1875@gmail.com>
2023-10-23 17:17:18 +0900

    test: Array, Set, Map should be iterable

--- src/1.iteration-test.js
+++ src/1.iteration-test.js
@@ -1,7 +1,16 @@
 import { describe, it } from 'node:test'
+import assert from 'node:assert'
 
 describe('Array, Set, Map', () => {
-  it.todo('should be iterable')
+  it('should be iterable', () => {
+    const array = new Array() // equal to []
+    const set = new Set()
+    const map = new Map()
+
+    assert.notStrictEqual(array[Symbol.iterator], undefined)
+    assert.notStrictEqual(set[Symbol.iterator], undefined)
+    assert.notStrictEqual(map[Symbol.iterator], undefined)
+  })
 
   it.todo('should have the well-formed iterator')
 })
